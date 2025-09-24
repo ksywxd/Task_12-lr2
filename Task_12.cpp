42313d36bdc5675020bd98fdb46ecb42ca95646e
ksywxd <xenia5224@gmail.com>
2025-09-24 20:12:25 +0300

    kvad

--- Task_12.cpp
+++ Task_12.cpp
@@ -13,3 +13,28 @@ double cor(double x) {
     return mid;
 }
 
+void kvad(double a, double b, double c, double& x1, double& x2) {
+    double D = b * b - 4 * a * c;
+    if (D < 0) {
+        std::cout << "Нет действительных корней.\n";
+        return;
+    }
+    else if (D == 0) {
+        x1 = -b / (2 * a);
+    }
+    else {
+        double y1 = (-b + cor(D)) / (2 * a);
+        double y2 = (-b - cor(D)) / (2 * a);
+    }
+}
+
+void solveType2(double a, double b, double c) {
+    std::cout << "\nТип 2: Симметричное уравнение\n";
+    if (a == 0) {
+        std::cout << "Это не симметричное уравнение.\n";
+        return;
+    }
+
+    // az^2 + bz + (c-2a) = 0
+
+    
