6ea18a4482c8218f672089ffb2b18b597463038c
ksywxd <xenia5224@gmail.com>
2025-09-24 19:35:36 +0300

    Добавьте файлы проекта.

---
+++ Task_12.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+double cor(double x) {
+    if (x < 0) return -1;
+    double low = 0, high = x >= 1 ? x : 1, mid;
+    for (int i = 0; i < 100; i++) {
+        mid = (low + high) / 2;
+        double square = mid * mid;
+        if (square - x < 1e-10 && square - x > -1e-10) return mid;
+        if (square < x) low = mid;
+        else high = mid;
+    }
+    return mid;
+}
+
